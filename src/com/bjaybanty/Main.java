b8f2d931c33a300ecce3fbc01d2073843b64112d
bolajionikoyi <bolajionikoyi@gmail.com>
2022-01-17 15:22:52 -0600

    initial commit

---
+++ src/com/bjaybanty/Main.java
@@ -0,0 +1,11 @@
+package com.bjaybanty;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Product p = new Product("PS5");
+        System.out.println(p.getName());
+
+    }
+}
